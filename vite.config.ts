24fc1e5c8b8299c0dfe4f4b0067e0f8e65427230
Jared Cole <jaredrcole@gmail.com>
2023-08-11 16:41:15 -0500

    issue/3 (#4)
    https://github.com/dontforgetjared/marvel-characters-app/issues/3
    
    * Adds vite ssl plugin
    * Adds meta tag to html to upgrade insucure requests

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/marvel-characters-app/',
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
 });
