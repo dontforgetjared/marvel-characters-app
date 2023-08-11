84d94de0f0ffd109d1f829870cb29c043c5de79d
Jared Cole <jaredrcole@gmail.com>
2023-08-11 14:25:17 -0500

    Merges fix/eslint-ts
    Squashed commit of the following:
    
    commit 53d48495ea03eb29191a00da8b02d6bc8a0140f5
    Author: Jared Cole <jaredrcole@gmail.com>
    Date:   Fri Aug 11 14:23:27 2023 -0500
    
        fix/eslint-ts
        *  Removing eslint from vite config to see if it fixes build issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/marvel-characters-app/',
-  plugins: [
-    react(),
-    eslint({
-      cache: false,
-      include: ['./src/**/*.ts', './src/**/*.tsx'],
-    }),
-  ],
+  plugins: [react()],
 });
